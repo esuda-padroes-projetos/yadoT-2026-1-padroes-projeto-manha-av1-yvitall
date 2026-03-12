2cb383f683a0a0c0c3508a04af68e5e0942d8804
yvitall <yuritsvital05@gmail.com>
2026-03-12 14:13:42 -0300

    habitModel

--- backend-yadot-api/src/main/java/com/yadot/api/model/HabitModel.java
+++ backend-yadot-api/src/main/java/com/yadot/api/model/HabitModel.java
@@ -5,16 +5,16 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter // Lombok
-@Entity // Transforma a classe em uma entidade no banco de dados
+@Entity // Transforma a classe em uma entidade no banco de dados (necessario em classesModel/Entities)
 @Table(name = "tb_habitos") //criacao de uma tabela com nome da tabela
 
 public class HabitModel {
     @Id //add auto na tabela o id (dependency JavaPersistenceAPI/BD)
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long habitId;
-    String habitName;
-    String habitCategory;
-    String habitIcon;
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //forma de gerar o id (1, 2...)
+    private Long habitId;
+    private String habitName;
+    private String habitCategory;
+    private String habitIcon;
 
     public HabitModel() {
     }
